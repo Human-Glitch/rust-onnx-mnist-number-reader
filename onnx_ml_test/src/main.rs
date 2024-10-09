0daa69b3f262d30221b273420c659d046d71ae3e
Kody Buss <kody.buss@gmail.com>
2024-10-08 23:46:51 -0500

    built preprocess image logic using image library

--- onnx_ml_test/src/main.rs
+++ onnx_ml_test/src/main.rs
@@ -11,14 +11,19 @@ fn main() {
 }
 
 mod processor_utils {
-    use std::fs::read;
-
+    use image::open;
     use ort::{GraphOptimizationLevel, Session};
 
     const MODEL_PATH: &str = "src/models/mnist_model.onnx";
 
     pub fn preprocess_image(img_path: &str) -> Vec<u8> {
-        return Vec::new();
+        let img = open(&img_path).unwrap();
+        let gray_img = img.to_luma8();
+        //gray_img.save("output_gray.png").unwrap();
+
+        let ready_img: Vec<u8> = gray_img.into_raw();
+
+        return ready_img;
     }
 
     pub fn build_model() -> Session {
@@ -43,6 +48,16 @@ mod test {
     #[test]
     fn preprocess_image_when_handwritten_digit_5_jpg_then_is_gray_scale_vector(){
         let img_path = "test_data/handwritten_5.jpg";
+        let ready_img = preprocess_image(img_path);
+
+        for &value in &ready_img {
+            assert!(value >= 0 && value <= 255, "Value {} is out of bounds for grayscale!", value);
+        }
+    }
+
+    #[test]
+    fn preprocess_image_when_handwritten_digit_3_png_then_is_gray_scale_vector(){
+        let img_path = "test_data/handwritten_3.png";
         let ready_img = preprocess_image(img_path);
 
         for &value in &ready_img {
