45f234312730f6acdc3fc8b396b28c68f08483ba
Kody Buss <kody.buss@gmail.com>
2024-10-08 19:11:15 -0500

    Stub out some tests on expected outcome

--- onnx_ml_test/src/main.rs
+++ onnx_ml_test/src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// Stub out some tests
+#[cfg(test)]
+mod test {
+
+    #[test]
+    fn guess_when_handwritten_digit_5_jpg_then_label_5(){
+        let path = "test_data/handwritten_5.jpg".to_string();
+
+        // Do something here
+        let res = 0;
+
+        assert_eq!(res, 5);
+    }
+
+    #[test]
+    fn guess_when_handwritten_digit_3_png_then_label_3(){
+        let path = "test_data/handwritten_3.png".to_string();
+
+        // Do something here
+        let res = 0;
+
+        assert_eq!(res, 3);
+    }
+}
